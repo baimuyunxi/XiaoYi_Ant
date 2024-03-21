b5b29f01db3de5c3739bd54a29b0417c672bf2a5
baimuyunxi <1365402987@qq.com>
2024-03-22 00:24:56 +0800

    初始化

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import { DefaultFooter } from '@ant-design/pro-components';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  // @ts-ignore
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright="湖南客户服务中心"
+    />
+  );
+};
+
+export default Footer;
