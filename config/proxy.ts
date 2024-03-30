71f5336ee3ebd7afd4335ccda9df28415b087738
baimuyunxi <1365402987@qq.com>
2024-03-30 18:28:54 +0800

    build

--- config/proxy.ts
+++ config/proxy.ts
@@ -30,14 +30,14 @@ export default {
   test: {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
-      target: 'https://134.178.18.223:8080',
+      target: 'http://localhost:8066',
       changeOrigin: true,
       pathRewrite: {'^': ''},
     },
   },
   pre: {
     '/api/': {
-      target: 'https://134.178.18.223:8080',
+      target: 'http://localhost:8066',
       changeOrigin: true,
       pathRewrite: {'^': ''},
     },
